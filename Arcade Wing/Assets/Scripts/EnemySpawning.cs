409302be4e36bec19c4bf1b34a35710654ced1d2
WilliamMawleyMills <39174605+WilliamMawleyMills@users.noreply.github.com>
2018-05-23 16:35:31 +1000

    Fixed Enemy Spawning so that it handles at game start enemies better, as well as allowing the spawn locations and enemy spawning within a single function.

--- Arcade Wing/Assets/Scripts/EnemySpawning.cs
+++ Arcade Wing/Assets/Scripts/EnemySpawning.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class EnemySpawning : MonoBehaviour
 {
+    public int startEnemies = 10;
     public float timer;
     public float timerLength = 100f;
     public float timerAccelerator = 0.1f;
+    public float minimumDistance = 100f;
+    public float maximumDistance = 150f;
     public GameObject enemyPrefab;
     public Transform player;
     public Vector3 spawnLocation;
@@ -14,27 +17,33 @@ public class EnemySpawning : MonoBehaviour
     void Start()
     {
         timer = timerLength;
+        for (int i = 0; i < startEnemies; i++)
+        {
+            newSpawnLocation();
+        }
     }
 
     // Update is called once per frame
     void Update ()
     {
-       
+        
         if (timer <= 0f)
         {
             newSpawnLocation();
-            GameObject enemy = Instantiate(enemyPrefab, spawnLocation, Quaternion.identity) as GameObject;
             timer = timerLength;
             timerLength -= timerAccelerator;
         }
-        timer--;
+        timer -= 1 * Time.smoothDeltaTime;
 	}
     private void newSpawnLocation()
     {
-        spawnLocation = new Vector3(Random.Range(-150f, 150f), Random.Range(-150f, 150f), Random.Range(-150f, 150f));
-        if (Vector3.Distance(player.position, spawnLocation) < 100f)
+        spawnLocation = new Vector3(Random.Range(-maximumDistance, maximumDistance), Random.Range(-maximumDistance, maximumDistance), Random.Range(-maximumDistance, maximumDistance));
+        if (Vector3.Distance(player.position, spawnLocation) < minimumDistance)
         {
             newSpawnLocation();
+        } else
+        {
+            GameObject enemy = Instantiate(enemyPrefab, player.position + spawnLocation, Quaternion.identity) as GameObject;
         }
     }
 }
