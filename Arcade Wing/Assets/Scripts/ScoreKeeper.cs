33fc0018b8f6b88aacf9994de6fe8098cc786a42
WilliamMawleyMills <39174605+WilliamMawleyMills@users.noreply.github.com>
2018-05-31 17:04:42 +1000

    Score keeper properly added

---
+++ Arcade Wing/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        scoreText.text = score.ToString();
+
+    }
+
+    public void ScoreIncrement(int tally)
+    {
+        score += tally;
+    }
+}
